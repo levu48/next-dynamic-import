0b6092b779c6b589ba3d363364a2fc2e859c1669
Thach Le <levu48@gmail.com>
2017-07-19 10:22:59 -0700

    clean up genTable.js

--- genTable.js
+++ genTable.js
@@ -1,25 +1,18 @@
 import path from 'path';
 import fs from 'fs';
 
-console.log('>>> DIR', __dirname);
-
 let genScript = (str) => `import dynamic from 'next/dynamic';
 export default { 
 ${str}}
 `;
 
-let jsStr = "";
-
 fs.readdir(__dirname + '/components', (error, items) => {
     let str = "";
-    console.log(items);
     items.forEach(item => {
         item = item.slice(0, item.length-3);
         str += `'components/${item}': () => dynamic(import('./components/${item}')),\n`;
     });
-    jsStr = genScript(str);
-    console.log('>>> JS STR', jsStr);
-    fs.writeFileSync(__dirname + '/componentTable.js', jsStr, (err) => {
+    fs.writeFileSync(__dirname + '/componentTable.js', genScript(str), (err) => {
         console.log(err);
     });
 });
