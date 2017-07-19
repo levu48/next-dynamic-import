32a36e34ccbcd77013c6cd06d4a20b2295a68f7e
Thach Le <levu48@gmail.com>
2017-07-19 13:06:36 -0700

    add query handler to index page

--- components/ThirdPartyComponent.js
+++ components/ThirdPartyComponent.js
@@ -1,3 +1,7 @@
 import React from 'react';
 
-export default () => <h2>A Third Party component</h2>;
+export default class ThirdPartyComponent extends React.Component {
+    render() {
+        return <h2>A Third Party component</h2>;
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,20 @@ import loader from '../loader';
 
 export default class Home extends React.Component {
     render() {
-        const Comp = loader('components/ArticleList');
+        let str = this.props.url.query.c;
+        const Comp = str ? loader(str) : () => (
+            <div>
+                <div>Please provide a query for dynamic component,</div>
+                <div>e.g. localhost:3000?c=components/ThirdPartyComponent</div>
+            </div>
+        );
+
         return (
-            <Comp />
+            <div>
+                <Comp {...this.props} />
+                <div>Properties:</div>
+                <pre>{JSON.stringify(this.props, null, 2)}</pre>
+            </div>
         )
     }
 }
